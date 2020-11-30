581bf7dd17e75c25e9d3d8f64309c8a80d33f0a2
Armin Hadziahmetovic <hadziahmetovic@bio.ifi.lmu.de>
2020-11-30 09:26:36 +0100

    changed to alpine to resolve podman issue; added mkdir parent

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM openjdk:14
+FROM openjdk:14-alpine
+
+RUN apk add bash
 
 ADD scripts /home/scripts
 ADD software /home/software
--- scripts/empires_buildindex.sh
+++ scripts/empires_buildindex.sh
@@ -3,11 +3,12 @@
 gtf=$1
 fasta=$2
 fastaIndex=$3
-out=$4
+
+mkdir -p /home/data/indices/empires
 
 ## creates index
 java -cp /home/software/nlEmpiRe.jar nlEmpiRe.rnaseq.mapping.ExtractTranscriptomeInfo \
  -gtf $gtf \
  -genome $fasta \
  -genomeidx $fastaIndex \
- -o $out
+ -o /home/data/indices/empires/empires.index
